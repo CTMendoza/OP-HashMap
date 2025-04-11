5b2bc94435e77b2644910d3c1812b1b1b71dc675
Christopher Mendoza <tiptop1112@gmail.com>
2025-04-11 12:15:45 -0700

    Created the remove method for the HashMap class

--- hashMap.js
+++ hashMap.js
@@ -34,14 +34,60 @@ class HashMap {
         let bucket = this.hash(key)
         return this.buckets[bucket].getValue(key)
     }
+
+    //  takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
+    has(key) {
+        let bucket = this.hash(key)
+        return this.buckets[bucket].contains(key)
+    }
+
+    // takes a key as an argument. If the given key is in the hash map,
+    //  it should remove the entry with that key and return true. 
+    // If the key isn’t in the hash map, it should return false.
+    remove(key) {
+        let bucket = this.hash(key)
+        let currentNode = this.buckets[bucket].head
+        let previousNode 
+        if(currentNode === null) {
+            return false
+        }
+
+        while(currentNode !== null) {
+
+            if(currentNode.key === key) {
+                if(previousNode === undefined) {
+                    this.buckets[bucket].head = currentNode.nextNode
+                } else {
+                    previousNode.nextNode = currentNode.nextNode
+                }
+                return true
+            }
+            previousNode = currentNode
+            currentNode = currentNode.nextNode
+        }
+        // return currentNode
+    }
 }
 
 let h = new HashMap()
 
-h.set('zaramamalamanana', 4)
-console.log(h.buckets)
-h.set('zaramamalamanana', 5)
-console.log(h.buckets)
-h.set('bingbong', 'bongbing')
-console.log(h.buckets)
-console.log(h.get('bingbong'))
+
+h.set('apple', 'red')
+h.set('banana', 'yellow')
+h.set('carrot', 'orange')
+h.set('dog', 'brown')
+h.set('elephant', 'gray')
+h.set('frog', 'green')
+h.set('grape', 'purple')
+h.set('hat', 'black')
+h.set('ice cream', 'white')
+h.set('jacket', 'blue')
+h.set('kite', 'pink')
+h.set('lion', 'golden')
+console.log(JSON.stringify(h.buckets, null, 2))
+h.remove("hat");
+console.log(JSON.stringify(h.buckets, null, 2))
+console.log(h.get("hat"))
+
+
+
